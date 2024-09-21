a47f167cfbeebbc147ab1dc9b9c556320cae4b98
Simon Brook <simon@datanauts.co.uk>
2024-09-21 17:31:45 +0100

    don't test for client id

--- spec/hubspot/config_spec.rb
+++ spec/hubspot/config_spec.rb
@@ -26,14 +26,6 @@ RSpec.describe Hubspot do
       expect(Hubspot.config.portal_id).to eq('test_portal_id')
     end
 
-    it 'sets and retrieves the client_id via the Hubspot module' do
-      Hubspot.configure do |config|
-        config.client_id = 'test_client_id'
-      end
-
-      expect(Hubspot.config.client_id).to eq('test_client_id')
-    end
-
     it 'sets and retrieves the client_secret via the Hubspot module' do
       Hubspot.configure do |config|
         config.client_secret = 'test_client_secret'
