fc00559cbfbd30ea5649495dcac6532362202566
Simon Brook <simon@datanauts.co.uk>
2024-09-28 04:13:47 +0100

    Adds create and archive methods to batches

--- lib/hubspot/batch.rb
+++ lib/hubspot/batch.rb
@@ -24,6 +24,7 @@ module Hubspot
   end
 
   # Class to handle batch updates of resources
+  # rubocop:disable Metrics/ClassLength
   class Batch < ApiClient
     attr_accessor :id_property, :resources, :responses
 
@@ -39,28 +40,25 @@ module Hubspot
     end
     # rubocop:enable Lint/MissingSuper
 
-    # Save the batch and return true/false based on whether it was successful
-    def save(endpoint: 'update')
-      resource_type = check_single_resource_type
-      inputs = gather_inputs
-
-      return false if inputs.empty? # Guard clause
-
-      # Perform the batch updates in chunks based on the resource type's limit
-      batch_limit = batch_size_limit(resource_type)
-      inputs.each_slice(batch_limit) do |batch_inputs|
-        response = batch_request(resource_type, batch_inputs, endpoint)
-        @responses << response
-      end
-
-      # Check if any responses failed
-      !any_failed?
+    # batch create from the resources
+    def create
+      save(action: 'create')
     end
 
-    # Upsert method that calls save with upsert endpoint
+    def update
+      # validate_update_conditions
+      save(action: 'update')
+    end
+
+    # Upsert method that calls save with upsert action
     def upsert
       validate_upsert_conditions
-      save(endpoint: 'upsert')
+      save(action: 'upsert')
+    end
+
+    # Archive method
+    def archive
+      save(action: 'archive')
     end
 
     # Check if all responses were successful
@@ -78,8 +76,6 @@ module Hubspot
       @responses.any? { |response| !response.all_successful? && !response.partial_success? }
     end
 
-    private
-
     def add_resource(resource)
       if @resources.any? && @resources.first.resource_name != resource.resource_name
         raise ArgumentError, 'All resources in a batch must be of the same type'
@@ -87,6 +83,30 @@ module Hubspot
 
       @resources << resource
     end
+
+    private
+
+    # rubocop:disable Metrics/MethodLength
+    def save(action: 'update')
+      @action = action
+      resource_type = check_single_resource_type
+      inputs = gather_inputs
+
+      return false if inputs.empty? # Guard clause
+
+      # Perform the batch updates in chunks based on the resource type's limit
+      batch_limit = batch_size_limit(resource_type)
+      inputs.each_slice(batch_limit) do |batch_inputs|
+        response = batch_request(resource_type, batch_inputs, action)
+        @responses << response
+      end
+
+      process_responses unless @action == 'archive'
+
+      # Check if any responses failed
+      !any_failed?
+    end
+    # rubocop:enable Metrics/MethodLength
 
     def check_single_resource_type
       raise 'Batch is empty' if @resources.empty?
@@ -96,6 +116,8 @@ module Hubspot
 
     # Gather all the changes, ensuring each resource has an id and changes
     def gather_inputs
+      return gather_archive_inputs if @action == 'archive'
+
       @resources.map do |resource|
         next if resource.changes.empty?
 
@@ -107,14 +129,24 @@ module Hubspot
       end.compact   # Removes nil entries
     end
 
+    # Gather inputs for the archive operation
+    def gather_archive_inputs
+      @resources.map do |resource|
+        {
+          id: resource.public_send(@id_property),   # Use the ID or the custom property
+          idProperty: determine_id_property         # Include idProperty if it's not "id"
+        }.compact
+      end.compact
+    end
+
     # Only include idProperty if it's not "id"
     def determine_id_property
       @id_property == 'id' ? nil : @id_property
     end
 
-    # Perform batch request based on the provided endpoint (upsert or update)
-    def batch_request(type, inputs, endpoint)
-      response = post("/crm/v3/objects/#{type}/batch/#{endpoint}", body: { inputs: inputs })
+    # Perform batch request based on the provided action (upsert, update, create, or archive)
+    def batch_request(type, inputs, action)
+      response = self.class.post("/crm/v3/objects/#{type}/batch/#{action}", body: { inputs: inputs }.to_json)
       BatchResponse.new(response.code, handle_response(response))
     end
 
@@ -122,14 +154,87 @@ module Hubspot
     def validate_upsert_conditions
       raise ArgumentError, "id_property cannot be 'id' for upsert" if @id_property == 'id'
 
-      if @resources.any? { |resource| resource.public_send(id_property).blank? }
-        raise ArgumentError, "All resources must have a non-blank value for #{@id_property} to perform upsert"
-      end
+      # check if there are any resources without a value from the id_property
+      return unless @resources.any? { |resource| resource.public_send(id_property).blank? }
+
+      raise ArgumentError, "All resources must have a non-blank value for #{@id_property} to perform upsert"
     end
 
     # Return the appropriate batch size limit for the resource type
     def batch_size_limit(resource_type)
       resource_type == 'contacts' ? CONTACT_LIMIT : DEFAULT_LIMIT
     end
+
+    # Process responses from the batch API call
+    def process_responses
+      @responses.each do |response|
+        next unless response['results']
+
+        process_results(response['results'])
+      end
+    end
+
+    # Process each result and update the resource accordingly
+    def process_results(results)
+      results.each do |result|
+        resource = find_resource_from_result(result)
+        next unless resource
+
+        # Set the ID on the resource directly
+        resource.id = result['id'].to_i if result['id']
+
+        # Update the resource properties
+        update_resource_properties(resource, result['properties'])
+
+        # Update metadata like updatedAt
+        update_metadata(resource, result['updatedAt'])
+      end
+    end
+
+    def find_resource_from_result(result)
+      case @action
+      when 'update', 'upsert'
+        find_resource_from_id(result['id'].to_i)
+
+      #
+      # when specifying idProperty in the upsert request
+      # the Hubspot API returns the id value (aka the hs_object_id)
+      # instead of the value of the <idProperty> field, so the value of the field
+      # is only stored in results['properties'][@id_property] if it changed!
+      # so this condition is redundant but left here in case Hubspot updates the response
+      #
+      # when 'upsert'
+      #   resource_id = result.dig('properties', @id_property) || result['id']
+      #   find_resource_from_id_property(resource_id)
+      #
+      when 'create'
+        # For create, check if the resource's changes are entirely contained in the result's properties
+        @resources.reject(&:persisted?).find do |resource|
+          resource.changes.any? && resource.changes.all? { |key, value| result['properties'][key.to_s] == value }
+        end
+      end
+    end
+
+    def find_resource_from_id(resource_id)
+      @resources.find { |r| r.id == resource_id }
+    end
+
+    # def find_resource_from_id_property(resource_id)
+    #   @resources.find { |r| r.public_send(@id_property) == resource_id }
+    # end
+
+    def update_resource_properties(resource, properties)
+      properties.each do |key, value|
+        if resource.changes[key]
+          resource.properties[key] = value
+          resource.changes.delete(key)
+        end
+      end
+    end
+
+    def update_metadata(resource, updated_at)
+      resource.metadata['updatedAt'] = updated_at if updated_at
+    end
   end
+  # rubocop:enable Metrics/ClassLength
 end
