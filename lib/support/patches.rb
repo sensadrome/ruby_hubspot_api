1517e8d7efdfc06f36bee992a118c2dbfbd236d9
Simon Brook <simon@datanauts.co.uk>
2024-10-01 03:35:55 +0100

    Yep. Back ported to 2.4

--- lib/support/patches.rb
+++ lib/support/patches.rb
@@ -9,3 +9,30 @@ class Object
     end
   end
 end
+
+# At some point this will seem like a bad idea ;)
+
+# :nocov:
+if RUBY_VERSION < '2.5.0'
+  class Hash
+    # Non-mutating version (returns a new hash with transformed keys)
+    def transform_keys
+      return enum_for(:transform_keys) unless block_given?
+      result = {}
+      each_key do |key|
+        result[yield(key)] = self[key]
+      end
+      result
+    end
+
+    # Mutating version (modifies the hash in place)
+    def transform_keys!
+      return enum_for(:transform_keys!) unless block_given?
+      keys.each do |key|
+        self[yield(key)] = delete(key)
+      end
+      self
+    end
+  end
+end
+# :nocov:end
