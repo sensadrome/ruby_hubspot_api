ed1fd74dca1bbc38e09b141116bf364b311e4509
Simon Brook <simon@datanauts.co.uk>
2024-10-14 20:31:50 +0100

    make erb explicitly determined by the file exension (.json.erb)

--- spec/support/fixture_helper.rb
+++ spec/support/fixture_helper.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 
 module FixtureHelper
+  FILE_NOT_FOUND_MSG = 'Fixture not found: %<name>s'
+
   def load_json(name)
-    file_path = File.join('spec', 'fixtures', "#{name}.json")
-    erb_template = ERB.new(File.read(file_path))
-    json_string = erb_template.result(binding)
+    erb_file_path = fixture_path("#{name}.json.erb")
+    json_file_path = fixture_path("#{name}.json")
+
+    json_string = if File.exist?(erb_file_path)
+                    parse_erb(erb_file_path)
+                  elsif File.exist?(json_file_path)
+                    File.read(json_file_path)
+                  else
+                    raise format(FILE_NOT_FOUND_MSG, name: name)
+                  end
+
     JSON.parse(json_string)
+  end
+
+  private
+
+  def fixture_path(filename)
+    File.join('spec', 'fixtures', filename)
+  end
+
+  def parse_erb(file_path)
+    ERB.new(File.read(file_path)).result(binding)
   end
 end
 
